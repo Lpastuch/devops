7ca4e2ca592507b3f45010c7093357025b9781b0
Lpastuch <l.pastuch@hotmail.com>
2026-09-14 17:12:19 -0300

    docker

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
+import random
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 # 127.0.0.1:8000/teste1
 @app.get("/teste1")
 async def funcaoteste():
     return {"glauglau": "esteéomain"}
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_item(id_estudante: int):
+    return id_estudante > 0
